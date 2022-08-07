a12530fd4b6ba4e88d1b3c098187047ca2e37f2d
Arav <arav@gmail.com>
2022-08-07 01:01:30 -0700

    done

--- content.js
+++ content.js
@@ -1,3 +1,25 @@
-console.log("hello")
-var elem = document.getElementById("myDiv");
-console.log("hello")
+// var content = document.getElementsByTagName('p')
+// content.remove()
+const delay = 30000
+
+function delete_Elements(tag) {
+
+var content = document.getElementsByTagName(tag)
+for (let elem = 0; elem < content.length; elem++) {
+    setTimeout(() => {
+        content[elem].parentNode.remove()
+    }, delay * (elem + 1))
+}
+console.log('ran')
+}
+
+delete_Elements("h1")
+delete_Elements("h2")
+delete_Elements("h3")
+delete_Elements("h4")
+delete_Elements("p")
+delete_Elements("span")
+delete_Elements("article")
+delete_Elements("images")
+delete_Elements("body")
+
